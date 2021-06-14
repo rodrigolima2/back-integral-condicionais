fd1e078db06ae950b2340f1637d3ec95090b360f
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 20:53:04 -0300

    Extrato de Compra Online

--- extra-17/index.js
+++ extra-17/index.js
@@ -1,8 +1,19 @@
-//valor do produto comprado.
-const valorDoProduto = 100000;
+//valor do produto comprado em centavos
+const valorDoProduto = 300000;
 
 //quantidade de parcelas
 const quantidadeDoParcelamento = 10;
 
-//valor pago
-const valorPago = 300;
+//valor pago em reais
+const valorPago = 2400;
+
+const valorDoProdutoEmReais = valorDoProduto / 100;
+
+if(valorPago < valorDoProdutoEmReais){
+    const valorDeCadaParcela = valorDoProdutoEmReais / quantidadeDoParcelamento;
+    const quantidadeDeParcelasRestantes = (valorDoProdutoEmReais - valorPago) / valorDeCadaParcela;
+
+    console.log(`Restam ${quantidadeDeParcelasRestantes} parcelas de ${(valorDeCadaParcela).toFixed(2)}`);
+} else {
+    console.log("O produto já foi pago.")
+}
