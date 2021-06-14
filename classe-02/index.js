02dc34c6a19a0a804ca2a2a0ecdcd045ffb9bd56
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 17:49:00 -0300

    Par ou ímpar

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,4 +1,15 @@
-const jogada1 = 5;
-const jogada2 = 3;
+const jogada1 = 4;
+const jogada2 = 2;
 
-//seu código aqui
+const resultado = jogada1 + jogada2;
+
+console.log("Par ou Ímpaaar!!");
+console.log(`O jogador 1 colocou ${jogada1}`);
+console.log(`O jogador 2 colocou ${jogada2}`);
+console.log("Calculando....");
+
+if(resultado % 2 == 0){
+    console.log(`O número ${resultado} é par, quem escolheu par vence!`);
+} else {
+    console.log(`O número ${resultado} é ímpar, quem escolheu ímpar vence!`);
+}
