71aad5c270bea8c7f7571d95769f7e9c67f4505e
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 20:07:45 -0300

    Compra com Desconto

--- extra-13/index.js
+++ extra-13/index.js
@@ -1,6 +1,18 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "debito";
 
 //valor da mercadoria (centavos)
-const valorDoProduto = 13000;
+const valorDoProduto = 20000;
 
+if(tipoDePagamento === "debito" || tipoDePagamento === "dinheiro"){
+    const valorDoProdutoComDesconto = (valorDoProduto * 0.90) / 100;
+    console.log(`Valor a ser pago: R$${(valorDoProdutoComDesconto).toFixed(2)}`);
+} else if(tipoDePagamento === "credito"){
+    const valorDoProdutoComDesconto = (valorDoProduto * 0.95) / 100;
+    console.log(`Valor a ser pago: R$${(valorDoProdutoComDesconto).toFixed(2)}`);
+} else if(tipoDePagamento === "cheque"){
+    const valorDoProdutoComDesconto = (valorDoProduto * 0.97) / 100;
+    console.log(`Valor a ser pago: R$${(valorDoProdutoComDesconto).toFixed(2)}`);
+} else{
+    console.log("Tipo de pagamento inválido");
+}
