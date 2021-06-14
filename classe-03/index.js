8a63cfe3c65ce1584736a6b68a97d5edf4eb9a55
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 17:57:20 -0300

    Dominó

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,4 +1,12 @@
-const ladoA = 3;
-const ladoB = 3;
+const ladoA = 4;
+const ladoB = 2;
 
-//seu código aqui
+console.log("Essa pedra é uma bucha?");
+console.log(`O lado A dessa pedra tem o valor de ${ladoA}`);
+console.log(`O lado B dessa pedra tem o valor de ${ladoB}`);
+
+if(ladoA === ladoB){
+    console.log("Sim, essa pedra é uma bucha!");
+} else{
+    console.log("Não, essa pedra não é uma bucha!");
+}
