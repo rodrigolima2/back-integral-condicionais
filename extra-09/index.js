6b6ddc45c1640549e94b4695192d61b309031f61
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 18:51:41 -0300

    Transformar nota em conceito

--- extra-09/index.js
+++ extra-09/index.js
@@ -1,1 +1,13 @@
-const nota = 8.5;
+const nota = 10;
+
+if(nota >= 9 && nota <= 10){
+    console.log("O estudante obteve conceito A");
+} else if(nota >= 8 && nota < 9){
+    console.log("O estudante obteve conceito B");
+} else if(nota >= 6 && nota < 8){
+    console.log("O estudante obteve conceito C");
+} else if(nota >= 4 && nota < 6){
+    console.log("O estudante obteve conceito D");
+} else if(nota < 4){
+    console.log("O estudante obteve conceito E");
+}
