77e6329191657d67563399dd574a6254fe8019c8
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-14 20:29:48 -0300

    Nome para exibição

--- extra-15/index.js
+++ extra-15/index.js
@@ -1,4 +1,13 @@
 const primeiroNome = "Mario";
 const sobrenome = "";
-const apelido = "";
+const apelido = "Ratata";
 
+if(apelido){
+    console.log(apelido);
+} else if(primeiroNome && sobrenome){
+    console.log(`${primeiroNome} ${sobrenome}`);
+} else if(primeiroNome){
+    console.log(primeiroNome);
+} else {
+    console.log(sobrenome);
+}
